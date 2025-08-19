3ed9f92cb99168c3941821b227234a2852c92df1
Alexander Guenther <alexander.guenther@iee-extern.fraunhofer.de>
2025-08-19 11:29:59 +0200

    decrytion and special letters

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -1,10 +1,15 @@
 import string
 
 all_letters = list(string.ascii_letters)
+all_letters.extend(["ä", "ö", "ü", "Ä", "Ö", "Ü", " "])
+all_letters.sort()
+
+print(all_letters)
 
 def encrypt(this_string: str, direction: int) -> str:
     return_string = ""
-    
+
+#    idea of mapping certain letters out    
 #    map = str.maketrans(" _", "_ ")
 #    direction.trandlate(map)
 
@@ -20,7 +25,13 @@ def encrypt(this_string: str, direction: int) -> str:
     
     return return_string
 
+def decrypt(this_string:str, direction:int) -> str:
+    return encrypt(this_string, direction*-1)
+
 ui_str = str(input("for encryption:"))
 ui_int = int(input("you direction:"))
+encrypted = encrypt(ui_str, ui_int)
+decrypted = decrypt(encrypted, ui_int)
 
-print(f"{ui_str}/{ui_int} -> {encrypt(ui_str, ui_int)}")
+print(f"{ui_str}/{ui_int} -> {encrypted}")
+print(f"{encrypted}/{ui_int*-1} -> {decrypted}")
