45ed6f4cd49dd125a24779830bcbd2961acebab8
Alexander Guenther <alexander.guenther@iee-extern.fraunhofer.de>
2025-08-19 13:10:19 +0200

    more letters in list

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -3,8 +3,9 @@ import string
 all_letters = list(string.ascii_letters)
 all_letters.extend(["ä", "ö", "ü", "Ä", "Ö", "Ü", " "])
 all_letters.sort()
+#print(all_letters)
 
-print(all_letters)
+running = True
 
 def encrypt(this_string: str, direction: int) -> str:
     return_string = ""
@@ -14,24 +15,35 @@ def encrypt(this_string: str, direction: int) -> str:
 #    direction.trandlate(map)
 
     for letter in this_string:
-        l_index = all_letters.index(letter)
-        
-        if l_index + direction >= len(all_letters):
-            l_index -= len(all_letters)
-        elif l_index + direction < 0:
-            l_index += len(all_letters)
-        
-        return_string += all_letters[l_index + direction]
+        if letter in all_letters:
+            l_index = all_letters.index(letter)
+            
+            if l_index + direction >= len(all_letters):
+                l_index -= len(all_letters)
+            elif l_index + direction < 0:
+                l_index += len(all_letters)
+            
+            return_string += all_letters[l_index + direction]
+        else:
+            # for letters not in list -> not encrypted
+            return_string += letter
     
     return return_string
 
 def decrypt(this_string:str, direction:int) -> str:
     return encrypt(this_string, direction*-1)
 
-ui_str = str(input("for encryption:"))
-ui_int = int(input("you direction:"))
-encrypted = encrypt(ui_str, ui_int)
-decrypted = decrypt(encrypted, ui_int)
+while running:
+    ui_str = str(input("for encryption:"))
+    ui_int = int(input("you direction:"))
+    encrypted = encrypt(ui_str, ui_int)
+    decrypted = decrypt(encrypted, ui_int)
 
-print(f"{ui_str}/{ui_int} -> {encrypted}")
-print(f"{encrypted}/{ui_int*-1} -> {decrypted}")
+    print(f"{ui_str}/{ui_int} -> {encrypted}")
+    print(f"{encrypted}/{ui_int*-1} -> {decrypted}")
+    
+    yes_or_no = str(input("want to proceed? (yes/no)"))
+    
+    if yes_or_no.lower() == "no":
+        running = False
+        print("BYE!!")
