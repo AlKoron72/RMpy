bf625990aa51fe4b8f8c4b75af2e487b58da4b83
Alexander Guenther <alexander.guenther@iee-extern.fraunhofer.de>
2025-08-25 10:41:14 +0200

    dagger as a weapon

--- weapons/dagger.py
+++ weapons/dagger.py
@@ -1,5 +1,9 @@
-from weapons.master.weapon import Weapon
+from master.weapon import Weapon
 
 class Dagger(Weapon):
     def __init__(self):
         super().__init__(name="Dagger", damage=15, weight=1.0, range=1)
+        
+if __name__ == "__main__":
+    dagger = Dagger()
+    print(dagger.attack())
--- weapons/master/weapon.py
+++ weapons/master/weapon.py
@@ -1,13 +1,22 @@
 class Weapon:
-    def __init__(self, name, damage, weight):
+    def __init__(self, name: str, weight: float=1.0, range: float=1.0, damage: float=10.0):
         self.name = name
-        self.damage = damage
-        self.weight = weight
-        self.fumble = 1  # Default fumble value
-        self.range = 1
+        self.damage = 10.0
+        self.weight = 1.0
+        self.fumble:int = 1  # Default fumble value
+        self.range = 1.0
+        self.damage_table = []  # Placeholder for damage table
+        self.special_properties = []
+        
         
     def __str__(self):
-        return f"Weapon: {self.name}, Damage: {self.damage}, Weight: {self.weight}, Fumble: {self.fumble}, Range: {self.range}"
+        return_value = f"Weapon: {self.name}\n"
+        return_value += f"Damage: {self.damage}\n"
+        return_value += f"Weight: {self.weight}\n"
+        return_value += f"Fumble: {self.fumble}\n"
+        return_value += f"Range: {self.range}\n"
+        
+        return return_value
 
     def attack(self):
         return f"{self.name} attacks with {self.damage} damage!"
